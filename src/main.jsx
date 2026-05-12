bd9bebe6411ee879f133f8aa9185cb87e0929b19
sumerian29 <sumerianur5@gmail.com>
2026-05-12 09:34:11 +0300

    Fix routing: add BrowserRouter and correct file extension

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'   // <-- أضف هذا السطر
+import './index.css'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter>      // <-- لف التطبيق بـ BrowserRouter
       <App />
     </BrowserRouter>
-  </StrictMode>
-);
+  </StrictMode>,
+)
