a46a6f8cf52635e81741423756c3f6a173c595ca
sumerian29 <sumerianur5@gmail.com>
2026-05-12 08:56:17 +0300

    Add react-router and separate employee/admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import DailyReportForm from './pages/DailyReportForm'
 import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <nav style={{ background: '#1e3a8a', padding: '10px', textAlign: 'center', direction: 'rtl' }}>
-          <Link to="/" style={{ color: 'white', margin: '0 15px', textDecoration: 'none' }}>نموذج الموظف</Link>
-          <Link to="/admin" style={{ color: 'white', margin: '0 15px', textDecoration: 'none' }}>لوحة المسؤول</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<DailyReportForm />} />
-          <Route path="/admin" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<DailyReportForm />} />
+      <Route path="/employee" element={<DailyReportForm />} />
+      <Route path="/admin" element={<Dashboard />} />
+    </Routes>
   )
 }
